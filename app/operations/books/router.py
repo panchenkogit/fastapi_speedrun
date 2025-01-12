4698f3289bdae1d1844b3fbe3d57cfc461586404
panchenkogit <odosaol@yandex.ru>
2025-01-12 22:01:21 +0500

    update gitignore+crud books

--- app/operations/books/router.py
+++ app/operations/books/router.py
@@ -1,47 +1,50 @@
 from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException
 
 from sqlalchemy import select, exists
 from sqlalchemy.ext.asyncio import AsyncSession
 
-
 from app.entities.books.model import Book as BookDB
 from app.entities.books.chema import Book, BookCreate, BookUpdate
- 
 
 from database.connect import get_db
 
 
-router = APIRouter(prefix="/book",
-                   tags=['Books'])
-
+router = APIRouter(prefix="/book", tags=['Books'])
 
 @router.get('/all')
 async def get_all_books(session: AsyncSession = Depends(get_db)) -> List[Book]:
     query = await session.execute(select(BookDB))
     result = query.scalars().all()
+
     if not result:
         raise HTTPException(status_code=404,
-        detail="Книги не найдены.")
+                            detail="Книги не найдены.")
 
     return result
 
 
-@router.get('/books/find/{parametr}')
+@router.get('/find/{parametr}')
 async def search_book(parametr: int | str, session: AsyncSession = Depends(get_db)):
-    book = await find_book(parametr, session)
+    try: 
+        book_id = int(parametr)
+        book = await find_book(book_id, session)
+
+    except ValueError:
+        book = await find_book(parametr, session)
+
     return book
 
 
-async def find_book(parametr: int | str,
-                    session: AsyncSession):
-
+async def find_book(parametr: int | str, session: AsyncSession) -> Book:
     if isinstance(parametr, int):
         query = await session.execute(select(BookDB).where(BookDB.id == parametr))
         result = query.scalar_one_or_none()
 
         if not result:
-            raise HTTPException(status_code=404, detail="Книга не найдена.")
+            raise HTTPException(status_code=404,
+                                detail="Книга не найдена.")
         
         return result
     
@@ -50,27 +53,21 @@ async def find_book(parametr: int | str,
         results = query.scalars().all()
 
         if not results:
-            raise HTTPException(status_code=404, detail="Книга не найдена.")
+            raise HTTPException(status_code=404,
+                                detail="Книга не найдена.")
         
         if len(results) > 1:
             book_titles = [{"id": book.id, "Название": book.title, "Автор": book.author} for book in results[:5]]
             raise HTTPException(status_code=400,
-                                detail={
-                                    "message": "Найдено несколько книг. Уточните запрос.",
-                                    "titles": book_titles
-                                })
+                                detail={"message": "Найдено несколько книг. Уточните запрос.",
+                                        "titles": book_titles})
         
         return results[0]
 
 
-
-
 @router.post("/add")
-async def add_book(book: BookCreate,
-                   session: AsyncSession = Depends(get_db)) -> Book:
-    
-    query = await session.execute(select(exists(BookDB).where(BookDB.author == book.author,
-                                                              BookDB.title == book.title)))
+async def add_book(book: BookCreate, session: AsyncSession = Depends(get_db)) -> Book:
+    query = await session.execute(select(exists(BookDB).where(BookDB.author == book.author, BookDB.title == book.title)))
     result = query.scalar()
 
     if result:
@@ -91,104 +88,37 @@ async def add_book(book: BookCreate,
     return new_book
 
 
-@router.delete("/delete/{id}")
-async def delete_by_id(id: int,
-                       session: AsyncSession = Depends(get_db)):
+@router.delete("/{id}")
+async def delete_book(id: int, session: AsyncSession = Depends(get_db)):
     query = await session.execute(select(BookDB).where(BookDB.id == id))
     result = query.scalar_one_or_none()
 
-    if result is None:
-       raise HTTPException(status_code=404,
-        detail="Книга не найдена.")
+    if not result:
+        raise HTTPException(status_code=404,
+                            detail="Книга не найдена.")
 
     await session.delete(result)
     await session.commit()
-    raise HTTPException(status_code=201,
-        detail="Книга удалена.")
+    return {"detail": f"Книга с ID {id} удалена."}
 
 
-
-@router.delete("/delete_by_title/{title}")
-async def delete_by_title(title: str, 
-                          session: AsyncSession = Depends(get_db)):
-
-    query = await session.execute(select(BookDB).where(BookDB.title.ilike(f"%{title}%")))
-    result = query.scalars().all()
-
-    if not result:
-        raise HTTPException(status_code=404, detail="Книга не найдена.")
-
-    if len(result) > 1:
-        book_titles = [{"Название" : book.title, "Автор": book.author} for book in result]
-        raise HTTPException(
-            status_code=400,
-            detail={
-                "message": "Найдено несколько книг. Уточните запрос.",
-                "titles": book_titles
-            })
-
-    book_to_delete = result[0]
-    await session.delete(book_to_delete)
-    await session.commit()
-
-    return {"detail": f"Книга '{book_to_delete.title}' удалена."}
-
-@router.patch('/books/{book_id}')
-async def update_book(book_id: int,
-                      book: BookUpdate,
-                      session: AsyncSession = Depends(get_db)):
-    
+@router.patch('/{book_id}')
+async def update_book(book_id: int, book: BookUpdate, session: AsyncSession = Depends(get_db)):
     query = await session.execute(select(BookDB).where(BookDB.id == book_id))
     result = query.scalar_one_or_none()
 
     if not result:
-        raise HTTPException(status_code=404, detail="Книга не найдена.")
-    
+        raise HTTPException(status_code=404,
+                            detail="Книга не найдена.")
+
     book_data = book.model_dump(exclude_unset=True)
+    if not book_data:
+        raise HTTPException(status_code=400,
+                            detail="Нет данных для обновления.")
+
     for key, value in book_data.items():
         setattr(result, key, value)
 
     await session.commit()
-    return result
-
-
-# Не самая лучшая практика
-# @router.patch('/update_book/{parametr}')
-# async def update_book(parametr: int | str,
-#                       book: BookUpdate,
-#                       session: AsyncSession = Depends(get_db)):
-
-
-#     if isinstance(parametr, int):
-#         query = await session.execute(select(BookDB).where(BookDB.id == parametr))
-#         result = query.scalar_one_or_none()
-#         if result is None:
-#             raise HTTPException(status_code=404, detail="Книга не найдена.")
-        
-
-#     elif isinstance(parametr, str):
-#         query = await session.execute(select(BookDB).where(BookDB.title.ilike(f"%{parametr}%")))
-#         results = query.scalars().all()
-
-#         if not results:
-#             raise HTTPException(status_code=404, detail="Книга не найдена.")
-        
-#         if len(results) > 1:
-#             book_titles = [{"id": book.id, "Название": book.title, "Автор": book.author} for book in results[:5]]
-#             raise HTTPException(
-#                 status_code=400,
-#                 detail={
-#                     "message": "Найдено несколько книг. Уточните запрос.",
-#                     "titles": book_titles
-#                 })
-        
-#         result = results[0]
-
-#     # Автоматическое обновление атрибутов
-#     book_data = book.model_dump(exclude_unset=True)
-#     for key, value in book_data.items():
-#         if hasattr(result, key):
-#             setattr(result, key, value)
-
-#     await session.commit()
-#     return result 
+    return {"detail": f"Книга с ID {book_id} обновлена.",
+            "data": book_data}
