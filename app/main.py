daeee243a9aa93f0daeb7d27a96d2f54e98c9b49
panchenkogit <odosaol@yandex.ru>
2025-01-14 11:38:16 +0500

    Добавление рандомных книг

--- app/main.py
+++ app/main.py
@@ -4,8 +4,12 @@ from app.operations.books.router import router as books_router
 from database.create_db import create_database
 
 app = FastAPI(title="FastAPI",
-              description="Тут будет описание",
-                version="1.1.0")
+              description="Короткое API для управления библиотекой.Позволяет добавлять, удалять, находить книги и управлять ими.",
+                version="1.2.0",
+                contact={
+        "name": "Евгений",
+        "email": "odosaol@yandex.ru",
+    },)
 
 app.include_router(books_router)
 
